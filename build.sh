95f984797c517182306cccc9507fc09e353447bc
unexploredtest <unexploredtest@tutanota.com>
2026-09-19 02:32:55 +0330

    Add support for aarch64

--- build.sh
+++ build.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
+# Set architecture from command-line argument or default to x86_64
+ARCH="${1:-x86_64}"
+
+# Validate architecture
+if [[ ! "$ARCH" =~ ^(x86_64|aarch64)$ ]]; then
+  echo "Error: Unsupported architecture '$ARCH'"
+  echo "Supported architectures: x86_64, aarch64"
+  exit 1
+fi
+
 TESTDISKURL="https://github.com/cgsecurity/testdisk"
-APPIMAGETOOLURL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage"
-LINUXDEPLOYURL="https://github.com/linuxdeploy/linuxdeploy/releases/latest/download/linuxdeploy-x86_64.AppImage"
-LINUXDEPLOYQTURL="https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/latest/download/linuxdeploy-plugin-qt-x86_64.AppImage"
+APPIMAGETOOLURL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-${ARCH}.AppImage"
+LINUXDEPLOYURL="https://github.com/linuxdeploy/linuxdeploy/releases/latest/download/linuxdeploy-${ARCH}.AppImage"
+LINUXDEPLOYQTURL="https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/latest/download/linuxdeploy-plugin-qt-${ARCH}.AppImage"
+
+echo "Building for architecture: $ARCH"
 
 wget "$APPIMAGETOOLURL"
-chmod a+x appimagetool-x86_64.AppImage
+chmod a+x appimagetool-${ARCH}.AppImage
 
 git clone "$TESTDISKURL" --depth 1
 cd testdisk
@@ -26,18 +38,18 @@ chmod a+x QPhotoRec.AppDir/usr/bin/qphotorec
 wget "$LINUXDEPLOYURL"
 wget "$LINUXDEPLOYQTURL"
 
-chmod a+x linuxdeploy-x86_64.AppImage
-chmod a+x linuxdeploy-plugin-qt-x86_64.AppImage
+chmod a+x linuxdeploy-${ARCH}.AppImage
+chmod a+x linuxdeploy-plugin-qt-${ARCH}.AppImage
 
 export QMAKE=/usr/bin/qmake6
 export NO_STRIP=1
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/lib:/usr/lib:/lib"
 
-./linuxdeploy-x86_64.AppImage \
+./linuxdeploy-${ARCH}.AppImage \
   --appdir QPhotoRec.AppDir \
   --desktop-file QPhotoRec.AppDir/usr/share/applications/qphotorec.desktop \
   --plugin qt
 
 cd ..
 
-./appimagetool-x86_64.AppImage testdisk/QPhotoRec.AppDir
+./appimagetool-${ARCH}.AppImage testdisk/QPhotoRec.AppDir
